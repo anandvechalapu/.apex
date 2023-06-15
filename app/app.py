20d9733508959173ac81fb30a3f09f99dea032f8
anandvechalapu <anand.vechalapu@gmail.com>
2023-06-15 12:17:19 +0530

    initial commit

---
+++ app/app.py
@@ -0,0 +1,7 @@
+Solution: 
+1. Create a Flask API that is compatible with Tableau’s requirements.
+2. The API should use the Tableau API to securely transmit data to Tableau. 
+3. Use the Tableau API to ensure accuracy and timeliness of the data.
+4. Test the API to ensure that the data is accurately transmitted to Tableau. 
+5. Deploy the API and make sure it is running properly. 
+6. Use a continuous integration process to ensure that the API is always running and up to date.
